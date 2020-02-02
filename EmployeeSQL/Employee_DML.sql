a1551ab353805c3ceed5d8ce9f4823557f71c9c6
NJ <nj@Nirmals-MBP.zyxel.com>
2020-02-01 22:37:37 -0500

    NJ - Wrapped up the queries

--- EmployeeSQL/Employee_DML.sql
+++ EmployeeSQL/Employee_DML.sql
@@ -14,5 +14,54 @@ where extract(year FROM "hire_date") = 1986;
 --List the manager of each department with the following information: department number, 
 --department name, the manager's employee number, last name, first name, and start and end employment dates.
 
-select * from employee_titles where title='Manager';
+select de.department_number, de.department_name,
+	   et.employee_number, es.first_name, es.last_name,
+	   et.from_date, et.to_date
+from employee_titles et 
+join employees es on es.employee_number = et.employee_number
+join employee_departments ed on ed.employee_number = et.employee_number
+join departments de on de.department_number = ed.department_number
+where et.title='Manager'
+order by de.department_number;
+
+--List the department of each employee with the following information: 
+--employee number, last name, first name, and department name.
+
+select de.department_number, es.employee_number, es.last_name,
+	   es.first_name, de.department_name
+from employees es
+join employee_departments ed on ed.employee_number = es.employee_number
+join departments de on de.department_number = ed.department_number;
+
+--List all employees whose first name is "Hercules" and last names begin with "B."
+
+select *
+from employees 
+where first_name = 'Hercules' and last_name like 'B%'
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+select es.employee_number, es.last_name,
+	   es.first_name, de.department_name
+from employees es
+join employee_departments ed on ed.employee_number = es.employee_number
+join departments de on de.department_number = ed.department_number and de.department_name = 'Sales';
+
+--List all employees in the Sales and Development departments, 
+--including their employee number, last name, first name, and department name.
+
+select es.employee_number, es.last_name,
+	   es.first_name, de.department_name
+from employees es
+join employee_departments ed on ed.employee_number = es.employee_number
+join departments de on de.department_number = ed.department_number and de.department_name in ('Sales', 'Development')
+order by de.department_name;
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
+select last_name, count(1) count_number_employees_by_last_name
+from employees 
+group by last_name order by count_number_employees_by_last_name desc;
+
+
 
